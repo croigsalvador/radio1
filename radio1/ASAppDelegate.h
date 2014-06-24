8aa9e64b92def5f94c1096a1d0abf3fe36f100a7
Alex Sanz <Alex.Sanz@sportingbet.com>
2014-06-24 10:22:55 +0100

    Project creation

---
+++ radio1/ASAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ASAppDelegate.h
+//  radio1
+//
+//  Created by Alex Sanz on 24/06/2014.
+//  Copyright (c) 2014 Alex Sanz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ radio1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  radio1
+//
+//  Created by Alex Sanz on 24/06/2014.
+//  Copyright (c) 2014 Alex Sanz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ASAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ASAppDelegate class]));
+    }
+}
