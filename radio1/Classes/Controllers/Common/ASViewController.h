835c5d8be88d8d33acf5c0111c215883e4b855a0
Alex Sanz <Alex.Sanz@sportingbet.com>
2014-06-24 10:52:09 +0100

    Base view controller creation

---
+++ radio1/Classes/Controllers/Common/ASViewController.h
@@ -0,0 +1,13 @@
+//
+//  ASViewController.h
+//  radio1
+//
+//  Created by Alex Sanz on 24/06/2014.
+//  Copyright (c) 2014 Alex Sanz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASViewController : UIViewController
+
+@end
---
+++ radio1/Classes/Controllers/Common/ASViewController.m
@@ -0,0 +1,25 @@
+//
+//  ASViewController.m
+//  radio1
+//
+//  Created by Alex Sanz on 24/06/2014.
+//  Copyright (c) 2014 Alex Sanz. All rights reserved.
+//
+
+#import "ASViewController.h"
+
+@interface ASViewController ()
+
+@end
+
+@implementation ASViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
