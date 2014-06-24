8cecb4dcbd06c3deb977e01fb9ac0f94fee4ddbb
Alex Sanz <Alex.Sanz@sportingbet.com>
2014-06-24 10:52:24 +0100

    Menu Radio List view controller creation

---
+++ radio1/Classes/Controllers/Menu/ASMenuViewController.m
@@ -0,0 +1,26 @@
+//
+//  ASMenuViewController.m
+//  radio1
+//
+//  Created by Alex Sanz on 24/06/2014.
+//  Copyright (c) 2014 Alex Sanz. All rights reserved.
+//
+
+#import "ASMenuViewController.h"
+
+@interface ASMenuViewController ()
+
+@end
+
+@implementation ASMenuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
